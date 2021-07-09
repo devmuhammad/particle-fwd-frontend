87163476a8309da4c154d780a22050bf16891767
devmoh-ulesson <moh@ulesson.com>
2021-07-09 18:42:28 +0100

    final changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { render } from './test-utils';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
-  expect(linkElement).toBeInTheDocument();
+import { getAllCars } from './services/api'
+import {carData} from './tests/data'
+const axios = require('axios');
+
+jest.mock('axios');
+
+test('returns car list from API', async () => {
+  axios.get.mockResolvedValue({
+    data: carData
+  })
+  const data = await getAllCars().then(resp => {return resp.data})
+  expect(data).toEqual(carData);
 });
+
+test('returns images', async () => {
+  axios.get.mockResolvedValue({
+    data: carData
+  })
+  const images = await getAllCars().then(resp => {return resp.data.images})
+  expect(images).toHaveLength(5);
+})
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const PARTICLE_API = 'http://localhost:9900/'
+const PARTICLE_API = 'https://particle-fwd-api.herokuapp.com/'
 
 export const getAllCars = () => {
       return axios.get(PARTICLE_API);
